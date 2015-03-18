04a4dc765e6923b17d28251e10737dd88d0756ef
Sophie <smrcmd@mst.edu>
2015-03-18 07:20:06 -0500

    CTS Challenge in Maven

---
+++ mavenCTSCC/src/main/java/com/mycompany/mavenctscc/codechallenge/Management.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenctscc.codechallenge;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sophie
+ */
+public class Management {
+    public int Management()
+    {
+        int managertotal = 0;
+        String response = null;
+        System.out.println("Do you currently manage any other managers? 'yes' or 'no'?");
+        Scanner sc = new Scanner(System.in);
+        response = sc.next();
+        int totalBudget = 0;
+        
+        if (response.equalsIgnoreCase("yes")) // do in a seperate class
+        {
+            System.out.println("How many managers are on this team?");
+            Scanner mgr = new Scanner(System.in);
+            managertotal = mgr.nextInt();
+            
+            int mgrcount = 1;
+            while(mgrcount <= managertotal)
+            {
+                System.out.println("For team " + mgrcount + ":");
+                NumberOfEmployees budget = new NumberOfEmployees();
+                mgrcount++;
+                totalBudget += (budget.NumberOfEmployees());
+            }
+            
+            System.out.println("For YOUR team:");
+            NumberOfEmployees budget = new NumberOfEmployees();
+            totalBudget += (budget.NumberOfEmployees());
+        }
+        else
+        {
+            NumberOfEmployees budget = new NumberOfEmployees();
+            totalBudget += (budget.NumberOfEmployees());
+        }
+      System.out.println("The total budget needed for this team is " + totalBudget);
+      return totalBudget; 
+    }
+}
