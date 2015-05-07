131effc4e01ab30fd553fa75c481dd7f4a114eb0
Sophie <smrcmd@mst.edu>
2015-05-07 18:03:56 -0500

    Caught the exceptions that were giving issues... Writes to a file. Reads from a file.

--- Asg2/src/umsl/AccountArray.java
+++ Asg2/src/umsl/AccountArray.java
@@ -20,73 +20,73 @@ public class AccountArray {
     {
         AccountArray aa = new AccountArray();
         
-        System.out.println("Is this your first time banking with Sophie Bank? 'yes' or 'no'");
+        System.out.println("Is this your first time banking with Sophie Bank? 'yes' or 'no':");
         Scanner response = new Scanner(System.in);
         String answer = response.nextLine();
 
         if(answer.equalsIgnoreCase("yes"))
         {
            aa.populateArray();
-           aa.readArray();
+           //aa.readArray();
            aa.selectAccount();
            aa.writeArray();
         }
-        else
+        
+        else 
         {
            aa.readArray();
            aa.selectAccount();
            aa.writeArray();
         }
-        
-        
+      
+//        System.out.println("Do you wish to make another transaction? 'yes' or 'no'");
+//        Scanner response2 = new Scanner(System.in);
+//        String answer2 = response2.nextLine();
+//        
+//        while(answer2.equalsIgnoreCase("yes"))
+//        {
+//           aa.readArray();
+//           aa.selectAccount();
+//           aa.writeArray();
+//        }
         //menu.Asg2Menu(balance); 
     }
     
-    public void populateArray() throws IOException, NotSerializableException, EOFException
+    public void populateArray() throws IOException, NotSerializableException, EOFException, InvalidClassException
     {
         for(int i=0; i<AcctArray.length; i++)
             {
                     AcctArray[i] = new Asg2();	
-                    AcctArray[i].setBalance(100);
+                    //AcctArray[i].setBalance(100);
             }
             
             System.out.println("There are " + AcctArray.length + " accounts available.");
             System.out.println();
             
-            for (int i=0; i<1; i++)
-                {
-                    AcctArray[i].showAll();
-                }
-            
-            try
-            {
-                OutputStream fout = new FileOutputStream("file.out");
-                OutputStream buffer = new BufferedOutputStream(fout);
-                ObjectOutputStream oout = new ObjectOutputStream(buffer);
-                oout.writeObject(AcctArray);
-                oout.flush();
-                fout.close();
-                System.out.println("Populate array has written to a file.");
-                
-            }
-            catch (Throwable f)
-            {
-                System.err.println(f);
-            }
+//            try
+//            {
+//                OutputStream fout = new FileOutputStream("file.out");
+//                OutputStream buffer = new BufferedOutputStream(fout);
+//                ObjectOutputStream oout = new ObjectOutputStream(buffer);
+//                oout.writeObject(AcctArray);
+//                oout.flush();
+//                fout.close();
+//                System.out.println("Populate array has written to a file.");
+//                
+//            }
+//            catch (Throwable f)
+//            {
+//                System.err.println(f);
+//            }
     } 
     
-    public void readArray() throws EOFException
+    public void readArray() throws EOFException, InvalidClassException
     {
         try
         {
             FileInputStream fIn = new FileInputStream("file.out");
             ObjectInputStream oIn = new ObjectInputStream(fIn);
             AcctArray = (Asg2[])oIn.readObject();
-            
-            for (int i=0; i<1; i++)
-                {
-                    AcctArray[i].showAll();
-                }
             
             fIn.close();
         }
@@ -97,7 +97,7 @@ public class AccountArray {
         System.out.println(AcctArray.length + " arrays to choose from.");
     }
 
-    public void writeArray()
+    public void writeArray() throws ArrayIndexOutOfBoundsException
     {
             try
             {
@@ -119,12 +119,23 @@ public class AccountArray {
      *
      * @return
      */
-    public void selectAccount()
+    public void selectAccount() throws ArrayIndexOutOfBoundsException
         {
-            System.out.println("Please enter the account you wish to access (0,1,2):" );
-            Scanner select = new Scanner(System.in);
-            int input = select.nextInt();
-            Asg2Menu menu = new Asg2Menu(); //call Asg2Menu from AccountArray
-            AcctArray[input].Asg2Menu();
+            int input = 0;
+            try
+            {
+                while (input == 0 || input == 1 || input == 2)
+                {
+                System.out.println("Please enter the account you wish to access (0,1,2) and -99 to exit:" );
+                Scanner select = new Scanner(System.in);
+                input = select.nextInt();
+                Asg2 menu = new Asg2(); //call Asg2Menu from AccountArray
+                AcctArray[input].Asg2Menu();
+                }
+            }
+            catch (Throwable o)
+                {
+                    System.exit(0);
+                }
         }
 }
